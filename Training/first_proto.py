c3212379c277172a1d1674d30734a2ce3c2bcc47
Chouettemortua <147599660+Chouettemortua@users.noreply.github.com>
2025-01-22 23:18:01 +0100

    comment part for training

--- Training/first_proto.py
+++ Training/first_proto.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bff02a51598bdfb2606d969605097722f83b2b7c40e1501859f36312687fa23
-size 4382
+oid sha256:272e492e0066eb861f881300174a4d74d2e2e8eec17c95ed36f204e98dabbaee
+size 4384
