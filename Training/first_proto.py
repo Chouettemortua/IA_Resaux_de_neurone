a3fee8ef6ba77b3f608313606fe7bdac27c3f387
Chouettemortua <147599660+Chouettemortua@users.noreply.github.com>
2025-01-22 18:23:58 +0100

    Neurone class implemation

--- Training/first_proto.py
+++ Training/first_proto.py
@@ -9,38 +9,47 @@ X,y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
 y = y.reshape((y.shape[0], 1))
 
 
-# Sert a l'initialisation du vecteur des paramétre sur les entrée
-# et la constante
-def initialisation(X):
-    W = np.random.randn(X.shape[1], 1)
-    b = np.random.randn(1)
-    return W,b
+class Neurone: 
+    # Sert a l'initialisation du vecteur des paramétre sur les entrée
+    # et la constante
+    def __init__(self,X,y):
+        self.W = np.random.randn(X.shape[1], 1)
+        self.b = np.random.randn(1)
+        self.L = []
+        self.neurone(X,y)
 
-# definit le modèle donc la fonction linéaire
-# puis transforme la sortie linéaire en un pourcentage
-def model(X,W,b):
-    Z = X.dot(W)+b
-    A = 1/(1+np.exp(-Z))
-    return A
+    # definit le modèle donc la fonction linéaire
+    # puis transforme la sortie linéaire en un pourcentage
+    def model(self,X):
+        Z = X.dot(self.W)+self.b
+        A = 1/(1+np.exp(-Z))
+        return A
 
-#On definit la fonction de coût donc log loss ici
-def log_loss(A,y):
-    return 1/len(y) * np.sum(-y*np.log(A) - (1-y)*np.log(1-A))
+    #On definit la fonction de coût donc log loss ici
+    def log_loss(self,A,y):
+        return 1/len(y) * np.sum(-y*np.log(A) - (1-y)*np.log(1-A))
 
-#On definit nos gradient
-def gradients(A, X, y):
-    dW = 1/len(y) * np.dot(X.T,A-y)
-    db = 1/len(y) * np.sum(A-y)
-    return dW,db
+    #On definit nos gradient
+    def gradients(self,A,X,y):
+        dW = 1/len(y) * np.dot(X.T,A-y)
+        db = 1/len(y) * np.sum(A-y)
+        return dW,db
 
-#On definit la decente de gradient pour ajuster les paramètre
-def update(dW, db, W, b, learning_rate):
-    W = W-learning_rate*dW
-    b = b-learning_rate*db
-    return W,b
+    #On definit la decente de gradient pour ajuster les paramètre
+    def update(self,dW, db, learning_rate):
+        self.W = self.W-learning_rate*dW
+        self.b = self.b-learning_rate*db
+
+    def neurone(self,X, y,learning_rate=0.1, nb_iter=100):
+        #boucle d'update
+        for _ in range(nb_iter):
+            A = self.model(X)
+            self.L.append(self.log_loss(A, y))
+            dW,db = self.gradients(A,X,y)
+            self.update(dW, db, learning_rate)
 
 
-#qq test basic
+"""#qq test basic
 W,b = initialisation(X)
 A = model(X,W,b)
 L = log_loss(A,y) 
@@ -54,7 +63,11 @@ assert(A.shape == (100, 1))
 assert(dW.shape == (2, 1))
 assert(db.shape == ())
 print("log loss:", L)
+"""
+neu = Neurone(X,y)
 
+plt.plot(neu.L)
+plt.show()
 
 #plt.scatter(X[:,0], X[:,1], c=y, cmap="summer")
 #plt.show()
