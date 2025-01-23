cae693786218f855e156c79bbcc45916fe337164
Chouettemortua <chouettemortua@gmail.com>
2025-01-23 17:35:13 +0100

    TEST-second-trainnig

--- Training/first_proto.py
+++ Training/first_proto.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:272e492e0066eb861f881300174a4d74d2e2e8eec17c95ed36f204e98dabbaee
-size 4384
+oid sha256:8dcfd17d65b192c10073b8851c4aa755887b8ab616c5856cd829a0e1ce3a9070
+size 4257
