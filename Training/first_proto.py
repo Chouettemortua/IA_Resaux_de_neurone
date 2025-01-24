81775fad08fc6efaf2e5766f850956d6955fb612
Chouettemortua <chouettemortua@gmail.com>
2025-01-24 01:28:55 +0100

    Update géneral - +comparaison de courbe - +ajustement de l'écriture du code

--- Training/first_proto.py
+++ Training/first_proto.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ccdbd754a058ba2ffbd2ab1c989f7e6dedecc65ffec29978970b0eaa9135648
-size 5301
+oid sha256:7eeb3463873557723e95ce703007795ded145efdb2c21cf85b41bf002cdf9ddb
+size 5851
