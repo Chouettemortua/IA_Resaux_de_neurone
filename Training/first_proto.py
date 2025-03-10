6d6cb9d13a0a0fdd4dcf4ac16f3c079ed3aa9151
Chouettemortua <chouettemortua@gmail.com>
2025-03-10 15:33:10 +0100

    test solution pour acc avec class_weight methode

--- Training/first_proto.py
+++ Training/first_proto.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49609c1982bfc90d50acd9c4d038323fbb930afae05283063062d8becce416c8
-size 9156
+oid sha256:b1f1e709456c471419d823c90d8fd0e56201773cfe32b1cde9e056f12adae3cf
+size 6706
