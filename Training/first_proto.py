acbbb10680e6dc47461b2885660fd673d512870f
Chouettemortua <chouettemortua@gmail.com>
2025-03-17 15:36:21 +0100

    preprocess presque fini pb reshape y

--- Training/first_proto.py
+++ Training/first_proto.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1f1e709456c471419d823c90d8fd0e56201773cfe32b1cde9e056f12adae3cf
-size 6706
+oid sha256:3e3505d27ddd8cb143150933a2e1c576889ba403191bb5f84b3d614ca57bfe89
+size 7071
