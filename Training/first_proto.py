1bf8d2bb21931c39da5ef5450ceea37a46827156
Chouettemortua <chouettemortua@gmail.com>
2025-04-28 15:29:12 +0200

    fenetre app base + actu sur l'ai resaux (pas réparer)

--- Training/first_proto.py
+++ Training/first_proto.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:380ffbeb6cdc59fb2820cd00094abb9cf525cec4ae5ec32431c555a6c94a9b35
-size 9811
+oid sha256:e92098c98e08f4904dbbd8b66f23f0cb567255c7d4d9eb0cccb9c142ef9212f6
+size 10614
