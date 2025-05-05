87beb2802c4e54bbd2f79fd3d0dfa8da18c7f3e2
Chouettemortua <chouettemortua@gmail.com>
2025-05-05 15:10:14 +0200

    base app done

--- Training/first_proto.py
+++ Training/first_proto.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8100b4ef10c0b65cf9d959d46df3a45fe36933fd63b6081d778224c9d84d2e49
-size 13804
+oid sha256:8f22dd9851fb212844e2ff990deab16daf2884c16d58db114a0d7c3d94e07e77
+size 13848
