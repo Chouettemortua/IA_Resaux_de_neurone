41385fe9b7ec57a12ca07d21ef57a3583e0fc703
Chouettemortua <chouettemortua@gmail.com>
2025-05-05 15:10:14 +0200

    base app done

--- Training/app.py
+++ Training/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a884d19e970976052544daa1c6d5e40e761e5ace380013e80119c50df209914
-size 395
+oid sha256:3b2884f8333927b5e0eea14fdd8a327da1c21350afdc354483f81350ea19d994
+size 9689
