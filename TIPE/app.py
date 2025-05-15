4b23df9c161b7d2a07c5b8ccb770583490601a72
Chouettemortua <chouettemortua@gmail.com>
2025-05-15 23:22:30 +0200

    Début refont UI

--- TIPE/app.py
+++ TIPE/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:704c1b2750f62255c11d0bd6a70614fc5d87357fc8d3ade41214581fcb462c7b
-size 10522
+oid sha256:11ad43b34a0a4fffac3475d74215f5b26a6b5102fd88a1e710cf1e106f095b02
+size 8463
