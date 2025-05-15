6a9340992af3c1f5b3598e7fb95387edd4f74d97
Chouettemortua <chouettemortua@gmail.com>
2025-05-15 23:22:30 +0200

    Début refont UI

--- TIPE/AI_Model.py
+++ TIPE/AI_Model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1d7771ea81f0ebbaedbf1bf7c8ae68480868fd91d9375afa1aaa0a5775cb634
-size 16434
+oid sha256:8131c78780406073aa76699631f630ef868843cfd50975e9e7df096b41a4bf47
+size 16654
