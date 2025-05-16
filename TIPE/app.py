2801ea3824afc4e2b80ca9fe4fd2d36d0969818e
Chouettemortua <chouettemortua@gmail.com>
2025-05-16 22:08:07 +0200

    lien ok logique de moyenne a changer

--- TIPE/app.py
+++ TIPE/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4539eab47f762c819ecd7ab29322c1f140822746f442f070c11e0600fae67e18
-size 11090
+oid sha256:4a2c5d061f0791e25e45a37ad529534e49e7ff879e11d764ac35bf4767f1dfc3
+size 11091
