7c26dd2ab3c590163c962f333aa1bd99ac568ca5
Chouettemortua <chouettemortua@gmail.com>
2025-05-17 17:27:30 +0200

    lien fait, preprocecing user good; moyenne fait sur les data de référence

--- TIPE/AI_Model.py
+++ TIPE/AI_Model.py
@@ -224,23 +224,41 @@ def preprocecing(df, on):
     """ Prétraite les données """
 
     def encodage(df):
-        """ Encode les variables catégorielles """
+        code_bmi = {'Normal': 1, 'Normal Weight': 1, 'Overweight': 2, 'Underweight': 3, 'Obese': 4}
+        code_gender = {'Male': 1, 'Female': 2}
+        code_occupation = {'Software Engineer': 1, 'Doctor': 2, 'Sales Representative': 3, 'Nurse': 4, 'Teacher': 5,
+                        'Scientist': 6, 'Engineer': 7, 'Lawyer': 8, 'Accountant': 9, 'Salesperson': 10, 'Manager': 11, 'Other': 12}
+        code_sleep_disorder = {'Normal': 1, 'Sleep Apnea': 2, 'Insomnia': 3}
 
-        code_bmi = {'Normal Weight': 0, 'Overweight': 1, 'Underweight': 2, 'Obesity': 3}
-        code_gender = {'Male': 0, 'Female': 1}
-        code_occupation = {'Software Engineer': 0, 'Doctor': 1, 'Sales Representative': 2, 'Nurse': 3, 'Teacher': 4,
-                        'Scientist': 5, 'Engineer': 6, 'Lawyer': 7, 'Accountant': 8, 'Salesperson': 9, 'Manager': 10}
-        code_sleep_disorder = {'Normal': 0, 'Sleep Apnea': 1, 'Insomnia': 2}
-        
+        df['Blood Pressure'] = df['Blood Pressure'].astype(str).str.split('/').str[0]
+        df['Blood Pressure'] = pd.to_numeric(df['Blood Pressure'], errors='coerce')
 
-        df['Blood Pressure'] = df['Blood Pressure'].str.split('/').str[0].astype(int)
-        df['Sleep Disorder'] = df['Sleep Disorder'].apply(lambda x: x if x in ['Sleep Apnea', 'Insomnia'] else 'Normal')
+        # Remplacer les valeurs nulles ou "None" dans Sleep Disorder par 'Normal'
+        df['Sleep Disorder'] = df['Sleep Disorder'].replace({'None': 'Normal'})
+        df['Sleep Disorder'] = df['Sleep Disorder'].fillna('Normal')
         
         # Mapper les colonnes catégorielles
-        df['BMI Category'] = df['BMI Category'].map(code_bmi).fillna(-1).astype(int)
-        df['Gender'] = df['Gender'].map(code_gender).fillna(-1).astype(int)
-        df['Occupation'] = df['Occupation'].map(code_occupation).fillna(-1).astype(int)
-        df['Sleep Disorder'] = df['Sleep Disorder'].map(code_sleep_disorder).fillna(-1).astype(int)
+        #print("BMI uniques reçus :", df['BMI Category'].unique())
+        df['BMI Category'] = df['BMI Category'].astype(str).str.strip().map(code_bmi)
+        if df['BMI Category'].isnull().any():
+            raise ValueError("Valeur invalide dans 'BMI Category'. Vérifiez vos entrées.")
+        df['BMI Category'] = df['BMI Category'].astype(int)
+
+        df['Gender']  = df['Gender'].astype(str).str.strip().map(code_gender)
+        if df['Gender'].isnull().any():
+            raise ValueError("Valeur invalide dans 'Gender'. Vérifiez vos entrées.")
+        df['Gender'] = df['Gender'].astype(int)
+
+        df['Occupation'] = df['Occupation'].astype(str).str.strip().map(code_occupation)
+        if df['Occupation'].isnull().any():
+            raise ValueError("Valeur invalide dans 'Occupation'. Vérifiez vos entrées.")
+        df['Occupation'] = df['Occupation'].astype(int)
+
+        #print("Sleep Disorder uniques reçus :", df['Sleep Disorder'].unique())
+        df['Sleep Disorder'] = df['Sleep Disorder'].astype(str).str.strip().map(code_sleep_disorder)
+        if df['Sleep Disorder'].isnull().any():
+            raise ValueError("Valeur invalide dans 'Sleep Disorder'. Vérifiez vos entrées.")
+        df['Sleep Disorder'] = df['Sleep Disorder'].astype(int)
 
         return df
 
@@ -284,7 +302,7 @@ def preprocecing_user(df, on=None):
     """ Prétraite les données """
 
     def encodage(df):
-        code_bmi = {'Normal': 1, 'Overweight': 2, 'Underweight': 3, 'Obesity': 4}
+        code_bmi = {'Normal': 1, 'Normal Weight': 1, 'Overweight': 2, 'Underweight': 3, 'Obese': 4}
         code_gender = {'Male': 1, 'Female': 2}
         code_occupation = {'Software Engineer': 1, 'Doctor': 2, 'Sales Representative': 3, 'Nurse': 4, 'Teacher': 5,
                         'Scientist': 6, 'Engineer': 7, 'Lawyer': 8, 'Accountant': 9, 'Salesperson': 10, 'Manager': 11, 'Other': 12}
@@ -295,8 +313,10 @@ def preprocecing_user(df, on=None):
 
         # Remplacer les valeurs nulles ou "None" dans Sleep Disorder par 'Normal'
         df['Sleep Disorder'] = df['Sleep Disorder'].replace({'None': 'Normal'})
+        df['Sleep Disorder'] = df['Sleep Disorder'].fillna('Normal')
         
         # Mapper les colonnes catégorielles
+        #print("BMI uniques reçus :", df['BMI Category'].unique())
         df['BMI Category'] = df['BMI Category'].astype(str).str.strip().map(code_bmi)
         if df['BMI Category'].isnull().any():
             raise ValueError("Valeur invalide dans 'BMI Category'. Vérifiez vos entrées.")
@@ -312,6 +332,7 @@ def preprocecing_user(df, on=None):
             raise ValueError("Valeur invalide dans 'Occupation'. Vérifiez vos entrées.")
         df['Occupation'] = df['Occupation'].astype(int)
 
+        #print("Sleep Disorder uniques reçus :", df['Sleep Disorder'].unique())
         df['Sleep Disorder'] = df['Sleep Disorder'].astype(str).str.strip().map(code_sleep_disorder)
         if df['Sleep Disorder'].isnull().any():
             raise ValueError("Valeur invalide dans 'Sleep Disorder'. Vérifiez vos entrées.")
@@ -327,7 +348,20 @@ def preprocecing_user(df, on=None):
     def normalisation(df):
         """ Normalise les données entre 0 et 1 """
 
-        return df/df.max()
+        df_ref = load('TIPE/datasets/Sleep_health_and_lifestyle_dataset.csv')
+        df_ref = df_ref.drop(columns=['Person ID'])
+
+        if on is not None and on in df_ref.columns:
+            df_ref = df_ref.drop(columns=on, axis=1)
+        else:
+            df_ref = df_ref.drop(columns=['Quality of Sleep'], axis=1)
+
+        df_ref = encodage(df_ref)
+        df_ref = imputation(df_ref)
+
+        max_values = df_ref.max()
+
+        return df.div(max_values)
     
     def intern(df):
         if on is not None and on in df.columns:
