15dbb4085f6e548f3c34dc44302b76620c63ee4f
Chouettemortua <chouettemortua@gmail.com>
2025-05-20 01:10:05 +0200

    Adapdation de résaux pour permettre les multiclass

--- TIPE/AI_Model.py
+++ TIPE/AI_Model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78ab758fa2f7bc522d1a4d2f1c66ec8e3fa65dfb44bd93f3e40d2a073611f3c9
-size 21697
+oid sha256:dc3996d3892b3e7718c1a59caf9bfe1999ad996aecb914de98c5d72db69ba17a
+size 24570
