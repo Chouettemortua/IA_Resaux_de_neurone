8da58d8375f58329282c8e4efa38c06019e696f3
Chouettemortua <chouettemortua@gmail.com>
2025-05-19 15:34:11 +0200

    19/05

--- TIPE/AI_Model.py
+++ TIPE/AI_Model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f4195337cb32dbf82b15d11299c2e4614a9f78c82a4dc5f43d1b5ebedd59efb
-size 21430
+oid sha256:78ab758fa2f7bc522d1a4d2f1c66ec8e3fa65dfb44bd93f3e40d2a073611f3c9
+size 21697
