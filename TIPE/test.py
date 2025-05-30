1550729459a88195d746dd27c4d9fa04031e3cee
Chouettemortua <chouettemortua@gmail.com>
2025-05-30 15:42:58 +0200

    analyse dataset

--- TIPE/test.py
+++ TIPE/test.py
@@ -1,15 +1,23 @@
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
+import AI_Model
+matplotlib.use('QtAgg')
 
 # Chargement des données
 df = pd.read_csv('TIPE/datasets/Sleep_health_and_lifestyle_dataset.csv')
+'''
+df = df.drop(columns=['Quality of Sleep'], axis=1)
+df = df.drop(columns=['Sleep Disorder'], axis=1)
+df = AI_Model.preprocecing_user(df)
+'''
 
 # Création de l'histogramme
 plt.figure(figsize=(10, 6))
-sns.histplot(df['Quality of Sleep'], bins=10, kde=True, color='skyblue')
-plt.title('Distribution de la qualité du sommeil')
-plt.xlabel('Note de qualité du sommeil')
+sns.histplot(df['Gender'], bins=10, kde=True, color='skyblue')
+plt.title('Distribution of Gender')
+plt.xlabel('Genre')
 plt.ylabel('Fréquence')
 plt.grid(True)
 plt.show()
