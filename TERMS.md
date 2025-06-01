4ac4c6449b4aee2fe0fa5f20e83971e48140f395
Chouettemortua <chouettemortua@gmail.com>
2025-06-01 18:05:40 +0200

    Un peu de documentation

---
+++ TERMS.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ef384a42caf85ee377cd08d3729ca377864160bd1c1004b1f8f559143851046d
+size 1734
--- TIPE/test.py
+++ TIPE/test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35d9ab70671731f9d019d5f56bdd5ab5ca8d8b2a498712db690e72be2c2e1cfc
-size 603
+oid sha256:bfc97aab4ee6b5e1ebd3ef79ec8c81cbcc28a79074b6acf21f9d8f7bb350d62f
+size 1703
