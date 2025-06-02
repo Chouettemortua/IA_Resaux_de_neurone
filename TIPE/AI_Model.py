6b4e364dee3f552ff61ebb7f3f3d50b0e88bd093
Chouettemortua <chouettemortua@gmail.com>
2025-06-03 01:43:52 +0200

    find profil optimal

--- TIPE/AI_Model.py
+++ TIPE/AI_Model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39a350acfa64ca4e1ce82a8fe6281474c4a6f3a905e296e70a4960e1a9f9ec8e
-size 27089
+oid sha256:470df073932ec4a3d56c13efce0dba811dad1ded2cbe6289e9b4bb07990a61a0
+size 28806
