9dd25fd5654a63f776d0d5bfdaefd8a2f8ff9d0a
Chouettemortua <chouettemortua@gmail.com>
2025-06-09 01:01:27 +0200

    ajustement de preprocesing

--- TIPE/AI_Model.py
+++ TIPE/AI_Model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b87b05f7ee07d98035a5207af20148d2bbbe599af87ae599acdbba0cad6f551
-size 30379
+oid sha256:a5321a4705ec7a3d7dcb3c8d0a7e2d8dee5931a06d34b162278aafa76e57c856
+size 31903
