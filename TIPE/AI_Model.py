ee6bb41e2daa0b71f2f93f3ffc36a4042bb3801c
Chouettemortua <chouettemortua@gmail.com>
2025-06-10 21:14:47 +0200

    qq modif (verbose et pdf)

--- TIPE/AI_Model.py
+++ TIPE/AI_Model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5321a4705ec7a3d7dcb3c8d0a7e2d8dee5931a06d34b162278aafa76e57c856
-size 31903
+oid sha256:3e550a38f158e7301481a8c1ce76ef881769e54f198f65ae1857a5b0ff4a6900
+size 31990
