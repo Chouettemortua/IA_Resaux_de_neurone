66e5a235e229fdd7761c9215a7a532d201f7c52a
Chouettemortua <chouettemortua@gmail.com>
2025-09-07 16:08:10 +0200

    Full run menu UI fait avec tout option prévu pour le moment

--- TIPE/Code/Core/training/AI_training_Quality.py
+++ TIPE/Code/Core/training/AI_training_Quality.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cc833e70a35629734ab4a53d287e996a1caea1c50c2d74021fc4f3c2572fd18
-size 3569
+oid sha256:9d3536dd9b74f0f81b053bad06dcce60aa6c7d0f402a7414a7ed3470fe7c40e4
+size 3516
