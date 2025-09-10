0d8a4ae6a68d2fe6e3d4a99d75612ac2a87a5ed8
Chouettemortua <chouettemortua@gmail.com>
2025-09-10 21:58:02 +0200

    minor change affichage graphique

--- TIPE/Code/Core/training/AI_training.py
+++ TIPE/Code/Core/training/AI_training.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d88cc6d5d6c59e01cb6a5f1e807e7337f4fdb8d6ec41793f0822ba065fba040f
-size 5489
+oid sha256:8707eba1249c2db9383a72c09926e9927a74d470d1cfba3090f3ea9ef8bf2dc1
+size 5372
