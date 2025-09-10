ecf0885b6b2ea50bb1999565bf279179c26ed293
Chouettemortua <chouettemortua@gmail.com>
2025-09-10 21:58:02 +0200

    minor change affichage graphique

--- TIPE/Code/Core/training/training_utils.py
+++ TIPE/Code/Core/training/training_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91d903cb6e8a1e2b1b007536ec0dddd8edf3de757bc05b31a9cf1d654af6cf5d
-size 7190
+oid sha256:2505ff2876fbe9a81fa434b77f205d38deef9f2930c93179632483c116d3c9c9
+size 7260
