7780d83a15d16ed699286570be1e2b49b76499b2
Chouettemortua <chouettemortua@gmail.com>
2025-09-12 18:47:03 +0200

    confusion matrix + quality to a class (because [0,10] is some classification]

--- TIPE/Code/Core/preprocessing/preprocessing.py
+++ TIPE/Code/Core/preprocessing/preprocessing.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94735888a51a13939029175576f3243feb97e6da9f72da431930e03af7cffb4a
-size 6862
+oid sha256:b5306c4a294e1244e133ed5cabb365645dac9a7713a079032bd629d8de89d60f
+size 6722
--- TIPE/Code/Core/training/AI_training.py
+++ TIPE/Code/Core/training/AI_training.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d4c156c84dfa479a445a17039c405754fb1d0e5421ae188eec6506380270201
-size 5381
+oid sha256:68337b32384ead8a47694a02e49f7dc676bddec42ba2a8c0b4ac3fcfb1e1c377
+size 5431
