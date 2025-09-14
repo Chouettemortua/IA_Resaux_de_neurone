e38707fa56c1f9af7f73d93467d0d6a4a1ce5396
Chouettemortua <chouettemortua@gmail.com>
2025-09-14 15:11:54 +0200

    Ajout de commentaire et de doc a toute les fonctions.

--- TIPE/Code/Core/preprocessing/preprocessing.py
+++ TIPE/Code/Core/preprocessing/preprocessing.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5306c4a294e1244e133ed5cabb365645dac9a7713a079032bd629d8de89d60f
-size 6722
+oid sha256:92dc01dc631953cb2d77d5bbb2e193c19c1d9117cb682877e5a9f0a7e9dfa3ac
+size 7918
--- TIPE/Code/Core/training/AI_training.py
+++ TIPE/Code/Core/training/AI_training.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68337b32384ead8a47694a02e49f7dc676bddec42ba2a8c0b4ac3fcfb1e1c377
-size 5431
+oid sha256:608cc09fed5b2a5537ee82d4d8529737adef8215d218ac96a7f24c5c2dc206bd
+size 5726
